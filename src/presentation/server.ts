0cea30e9a49690a2a1a21bbaa2b517da5b4b970f
Julio Jimenez <julius.1986@gmail.com>
2024-01-31 18:49:20 +0000

    fin seccion 14

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
   port: number;
   public_path?: string;
+  routes: Router
 }
 
 
@@ -12,11 +13,13 @@ export class Server {
   private app = express();
   private readonly port: number;
   private readonly publicPath: string;
+  private readonly routes: Router;
 
   constructor(options: Options) {
-    const { port, public_path = 'public' } = options;
+    const { port, public_path = 'public', routes } = options;
     this.port = port;
     this.publicPath = public_path;
+    this.routes = routes;
   }
 
   
@@ -26,8 +29,17 @@ export class Server {
 
     //* Middlewares
 
+    this.app.use( express.json() ); // raw
+    this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
+
+
     //* Public Folder
     this.app.use( express.static( this.publicPath ) );
+    
+
+
+    //* Routes
+    this.app.use(this.routes);
 
 
 
